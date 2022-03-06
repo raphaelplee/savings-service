d86b692233eb422f7363d8a160ddfe1af5e2161a
raphaelplee <62607433+raphaelplee@users.noreply.github.com>
2022-03-06 21:40:50 +0800

    Delete src/main directory

--- src/main/java/hello/config/MainConfig.java
+++
@@ -1,9 +0,0 @@
-package hello.config;
-
-import org.mybatis.spring.annotation.MapperScan;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@MapperScan(basePackages = "hello.dao")
-public class MainConfig {
-}
